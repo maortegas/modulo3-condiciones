228efe7edc8c4bcb12b1db032319a5d882374af3
Mauricio Ortega <mauricio.ortega.solis@gmail.com>
2023-07-12 11:13:08 -0400

    Add ejercicio3

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,12 @@ function addBorder(){
     let borderOff='0px none rgb(0, 0, 0)';
     let border = document.getElementById("img-xbox");
     let elementStyle = window.getComputedStyle(border);
-    let outputImagen= document.getElementById('img-xbox');
+
 
     if (elementStyle.border==borderOff ) {
-        outputImagen.style.border=borderOn;
+        border.style.border=borderOn;
     } else {
-        outputImagen.style.border=borderOff;
+        border.style.border=borderOff;
     }
 }
 
@@ -20,33 +20,33 @@ function cuentaStickers(){
     let inputC= Number(document.getElementById('c-cantidad').value);
     let inputJava= Number(document.getElementById('java-cantidad').value);
     let ouputTexto= document.getElementById('texto-cantidad');
-    ouputTexto.innerHTML='';  
 
     if (inputGo <0 || inputC<0 || inputJava<0 ){
-        ouputTexto.innerHTML='Debe de ingresar valores positivos';
+        ouputTexto.textContent='Debe de ingresar valores positivos';
         return
     }
 
     sumaSticker=inputGo + inputC+ inputJava
     if(sumaSticker>10) {
-        ouputTexto.innerHTML='Llevas demasiados stickers';   
+        ouputTexto.textContent='Llevas demasiados stickers';   
     }else{
-        ouputTexto.innerHTML='Llevas ' + sumaSticker + ' stickers';  
+        ouputTexto.textContent='Llevas ' + sumaSticker + ' stickers';  
     }
 }
 
 function validaPassword(){
-    const select1= document.getElementById('go-cantidad').value;
+    const select1= document.getElementById('select1').value;
+    const select2= document.getElementById('select2').value;
+    const select3= document.getElementById('select3').value;
 
-    switch (clave) {
-        case "911":
-          document.getElementById("mensaje").textContent = "password 1 correcto";
-          break;
-        case "714":
-          document.getElementById("mensaje").textContent = "password 2 correcto";
-          break;
-        default:
-          document.getElementById("mensaje").textContent = "password incorrecto";
-          break;
-      }
+    let pass=select1+select2+select3
+
+    if (pass=='911'){
+        document.getElementById("texto-password").textContent = "Password 1 correcto";
+    }
+    else if (pass=='714'){
+        document.getElementById("texto-password").textContent = "Password 2 correcto";
+    }else{
+        document.getElementById("texto-password").textContent = "Password incorrecto";   
+    }
 }
